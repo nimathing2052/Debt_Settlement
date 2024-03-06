cd435c600c81c690583c409f43109c7bd046a311
Nima Thing <nimathing2052@gmail.com>
2024-03-06 23:24:08 +0100

    Added debitor, creditor, and ammount and change in logic

--- Debt_Settlement/app.py
+++ Debt_Settlement/app.py
@@ -2,28 +2,40 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 def settle_debts(debts):
     transactions = []
 
-    for person, owes in debts.items():
-        for owed_to, amount in owes.items():
-            if person in debts.get(owed_to, {}) and debts[owed_to][person] >= amount:
-                transactions.append((person, owed_to, amount))
-                debts[owed_to][person] -= amount
-            else:
-                transactions.append((person, owed_to, amount))
+    for debtor, creditor_amounts in debts.items():
+        for creditor, amount in creditor_amounts.items():
+            transactions.append((debtor, creditor, amount))
 
     return transactions
+
 
 @app.route('/', methods=['GET', 'POST'])
 def debt_calculator():
     if request.method == 'POST':
-        debts = {}  # Structure to collect debts from the form (implementation below)
+        debts = {}
+        # Assuming form data will have specific naming convention: debtorX, creditorX, amountX
+        form_data = list(request.form.items())
+        num_entries = len(form_data) // 3
+
+        for i in range(num_entries):
+            debtor = request.form.get(f'debtor{i}')
+            creditor = request.form.get(f'creditor{i}')
+            amount = float(request.form.get(f'amount{i}', 0))
+
+            if debtor and creditor and amount:
+                if debtor not in debts:
+                    debts[debtor] = {}
+                debts[debtor][creditor] = debts[debtor].get(creditor, 0) + amount
 
         transactions = settle_debts(debts)
         return render_template('result.html', transactions=transactions)
     else:
         return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
