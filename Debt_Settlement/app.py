3580d04e2b2d33f5ff23465bfcfcdc9513f4100d
henrycgbaker <henry.c.g.baker@gmail.com>
2024-03-07 18:20:02 +0100

    css; user & debt DBs;
    super rough, will need rework.
    I also added comments for clarity / to structure app.py

--- Debt_Settlement/app.py
+++ Debt_Settlement/app.py
@@ -1,8 +1,13 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from sqlalchemy import select, text
 
+# APP INSTANCE
 app = Flask(__name__)
 
 
+# SETTLEMENT ALGO ------------------------------------------------------------------------------------------------------
 def settle_debts(debts):
     transactions = []
 
@@ -12,7 +17,50 @@ def settle_debts(debts):
 
     return transactions
 
+# DATABASE--------------------------------------------------------------------------------------------------------------
+# Configuring the SQLite database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
+# USER DATABSE
+# SQLAlchemy database instance
+db_user = SQLAlchemy(app)
+app.app_context().push()
+# Defining a model
+class UserInput(db_user.Model):
+    user_id = db.Column(db.Integer, primary_key=True) #HOW TO MAKE THIS SO IT IS AUTO ASSIGNED, NOT INPUT?
+    username_email = db.Column(db.String(20),nullable=False)
+    first_name = db.Column(db.String(20),nullable=False)
+    last_name = db.Column(db.String(20),nullable=False)
+    password = db.Column(db.String(db.String(20),nullable=False)
+    debts = db.relationship('DebtItem', backref='debt_id', lazy=True)
+
+    def __repr__(self):
+        return f"Users('{self.username_email}')"
+
+# DEBT DATABASE
+# SQLAlchemy database instance
+db_debt = SQLAlchemy(app)
+app.app_context().push()
+# Defining a model
+class DebtItem(db_debt.Model):
+    debt_id = db.Column(db.Integer, primary_key=True) #HOW TO MAKE THIS SO IT IS AUTO ASSIGNED, NOT INPUT?
+    item_name = db.Column(db.String(50), nullable=False)
+    payer = db.Column(db.String(50), nullable=False)
+    debtor_1 = db.Column(db.String(50),nullable=False)
+    debtor_2 = db.Column(db.String(50),nullable=True)
+    debtor_3 = db.Column(db.String(50),nullable=True)
+    monetary_value = db.Column(db.Integer(50), nullable=False) # HOW TO CHANGE TO FLOATING POINT?
+    group_id = db.Column(db.String(50), nullable=False)
+    #settle_debts
+    #currency
+    time_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"Debts('{self.item_name}', '{self.monetary_value}')"
+
+
+
+# PAGES ----------------------------------------------------------------------------------------------------------------
 @app.route('/', methods=['GET', 'POST'])
 @app.route("/home")
 def debt_calculator():
@@ -48,6 +96,13 @@ def login(
 def user_profile(
         title='Profile page'
         return render_template('user_profile.html',
+                               title=title)
+)
+
+@app.route("/debt_view"):
+def debt_view(
+        title='See all your debts, past and present!'
+        return render_template('debt_view.html',
                                title=title)
 )
 
