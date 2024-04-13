d4767abbba0a8b60b7cecf710708695b12dc97ba
Nima Thing <nimathing2052@gmail.com>
2024-04-13 02:34:16 +0200

    Added Ben's Code Completely

--- Debt_Settlement/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- Debt_Settlement/models/__init__.py
+++
@@ -1,1 +0,0 @@
-from Debt_Settlement import db
