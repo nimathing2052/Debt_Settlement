fec25edc3fd0898dd78aab14031b3d300cbd8e3a
Nima Thing <nimathing2052@gmail.com>
2024-04-23 19:34:09 +0200

    removed debt_cache

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
-from  import create_app
+from app import create_app
+from app.models import db  # Correctly import db from models package
 
 app = create_app()
 
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True, port=9999)
+    app.run(debug=True, port=9888)
